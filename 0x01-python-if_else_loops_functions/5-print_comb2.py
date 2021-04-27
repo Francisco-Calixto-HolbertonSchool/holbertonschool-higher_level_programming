1a62ea0ac1c7792a8d6c4d87a7813dc32a5bff13
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-04-27 17:05:25 +0000

    task 5 corection attempt

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
+for i in range(0, 99):
     if i < 10:
-        print('0' + '{}'.format(i), end = ", ")
-    elif i >= 10 and i < 99:
-        print(i, end = ", ")
-print(i)
+        i = '0' + str(i)
+    print(i, end=", ")
+print("{}".format(i + 1))
