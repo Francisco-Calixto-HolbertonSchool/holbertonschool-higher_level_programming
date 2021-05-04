e8beaaf979ee81a7ea140fe032c3a85b505c279a
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-04 21:31:39 +0000

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     aux = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             aux.append(True)
         else:
             aux.append(False)
