ac473c5ff9cd7be2b299eaae1af0a27bf2f798ff
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-05 18:23:57 +0000

    task 13 ft. maxi

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - entry point
+ * @head: head
+ * Return: 1 or 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+  listint_t *temp, *aux, *half;
+  int *a_1, *a_2, n, j, t, c, h;
+
+  if (!*head || !head)
+    return (1);
+  aux = *head;
+  for (n = 1; aux->next; n++)
+    aux = aux->next;
+  half = *head;
+  for (h = 0; h < ((n + 1) / 2); h++)
+    half = half->next;
+  a_1 = malloc(sizeof(int) * (n / 2));
+  if (!a_1)
+    return (-1);
+  a_2 = malloc(sizeof(int) * (n / 2));
+  if (!a_2)
+    return (-1);
+  temp = *head;
+  for (t = 0; t < (n / 2); t++)
+    {
+      a_1[t] = temp->n;
+      temp = temp->next;
+    }
+  for (j = ((n / 2) - 1); j >= 0; j--)
+    {
+      a_2[j] = half->n;
+      half = half->next;
+    }
+  for (c = 0; c < (n / 2); c++)
+    {
+      if (a_1[c] != a_2[c])
+	return (0);
+    }
+  return (1);
+}
