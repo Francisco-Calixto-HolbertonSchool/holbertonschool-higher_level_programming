dbdf01d7ab53af41b184d15d8b0f461902ee5873
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-06 22:22:41 +0000

    task 12 pepocho

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (isinstance(roman_string, str) == False) or roman_string == None:
+    if (not isinstance(roman_string, str)) or roman_string is None:
         return 0
-    dic = { 'X': 10, 'I': 1, 'C': 100, 'M': 1000, 'V': 5, 'L': 50, 'D': 500, 'O': 0 }
+    dic = {'X': 10, 'I': 1, 'C': 100, 'M': 1000, 'V': 5,
+           'L': 50, 'D': 500, 'O': 0}
     suma = 0
     i = 0
     aux = []
