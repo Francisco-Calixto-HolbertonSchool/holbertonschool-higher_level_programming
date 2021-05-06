77586df58e797afbfd4d33f304f917800f9225a4
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-06 22:19:38 +0000

    task 12 roman gg

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if (isinstance(roman_string, str) == False) or roman_string == None:
+        return 0
+    dic = { 'X': 10, 'I': 1, 'C': 100, 'M': 1000, 'V': 5, 'L': 50, 'D': 500, 'O': 0 }
+    suma = 0
+    i = 0
+    aux = []
+    for c in roman_string:
+        aux.append(c)
+    aux.append('O')
+    while dic[aux[i]] != 0:
+        if dic[aux[i]] >= dic[aux[i + 1]]:
+            suma += dic[aux[i]]
+        else:
+            suma -= dic[aux[i]]
+        i += 1
+    return suma
