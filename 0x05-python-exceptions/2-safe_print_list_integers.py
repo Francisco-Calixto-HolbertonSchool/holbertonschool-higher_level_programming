e938fdc7f8b7517d46ef6fd893bb4e37ad3f9bba
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-17 19:18:38 +0000

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
         except (TypeError, ValueError):
             i += 1
             continue
-        except IndexError:
-            break
     print("")
     return (count)
