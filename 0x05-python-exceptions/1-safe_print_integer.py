3d2f4f18055a257fc503e8a76f07a21edd2085ce
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-17 15:10:24 +0000

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    flag = 1
+    flag = True
     try:
         print("{:d}".format(value))
     except ValueError:
-        flag = 0
-    if flag == 1:
-        return True
-    else:
-        return False
+        flag = False
+    return flag
