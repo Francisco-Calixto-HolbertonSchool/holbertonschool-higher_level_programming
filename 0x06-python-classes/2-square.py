0c1e8cb941a9755d2177f99cc63854452af3a37f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-18 21:36:44 +0000

    task 2 correction gracias nacho

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,6 @@ class Square:
     __size = 0
 
     def __init__(self, size=0):
-        """Functon to do everything"""
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
