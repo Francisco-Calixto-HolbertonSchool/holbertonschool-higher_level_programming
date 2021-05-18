dd797736268b390b642c6f5749bf0e4f25e6cce5
Francisco-Calixto-HolbertonSchool <77985090+Francisco-Calixto-HolbertonSchool@users.noreply.github.com>
2021-05-18 18:55:49 -0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,15 @@
 #!/user/bin/python3
-"""Square class defined by size and verifies if size is valid"""
+"""Square class defined by size and"""
 
 
 class Square:
     """Inside the class yeahhh"""
-    __size = 0
 
     def __init__(self, size=0):
         """Functon to do everything"""
         if size is not None:
-            self.__size = size
             if not isinstance(self.__size, int):
                 raise TypeError('size must be an integer')
             if self.__size < 0:
                 raise ValueError('size must be >= 0')
+            self.__size = size
