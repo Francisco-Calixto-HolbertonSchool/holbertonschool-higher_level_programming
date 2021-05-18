1adc7a72a6a226a3b62bd8c8282e2292c44650d9
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-18 19:31:12 +0000

    task 1 with comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Task 2 square has a size"""
+
+
 class Square:
+    """Class that defines square by size"""
     __size = 0
+
     def __init__(self, new_size=None):
+        """abd ged ghg"""
         if new_size is not None:
             self.__size = new_size
