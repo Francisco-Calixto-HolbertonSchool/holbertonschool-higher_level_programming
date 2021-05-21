db99ebc775fd0a5bf5d47589ce9e8836a0b185a7
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-21 20:01:08 +0000

    task 3 .py and doctest

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""contains a function that takes name and las name and 
+prints with personalized message to stdout"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This functions does what the decription above says"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
