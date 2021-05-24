7a72dc51d8afe6035dfd9e691cdb59a1c1b2b7a7
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-24 14:40:25 +0000

    task 0 and readme

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module contains an empty class that defines a rectangle"""
+
+
+class Rectangle:
+    """Class defines rectangle, for now does nothing"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+0x08. Python - More Classes and Objects
+
+Similar to the other OOP project this one I will be working on a Rectangle class
