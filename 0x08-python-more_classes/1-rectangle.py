a4ba2c7fa6e921fe81e3fc622788e9a90bcc8af0
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-05-24 16:00:50 +0000

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module contains an empty class that defines a rectangle"""
+
+
+class Rectangle:
+    """Class defines rectangle, for now does nothing"""
+    __width = 0
+    __height = 0
+
+    def __init__(self, width=0, height=0):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
