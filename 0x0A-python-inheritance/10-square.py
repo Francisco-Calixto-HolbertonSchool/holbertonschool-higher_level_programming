cd564f6bfa0d900ced4e57899b16d5da68d9fb56
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-01 19:51:16 +0000

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains Square class"""
+
+
+Rectangle =  __import__('9-rectangle').Rectangle
+"""import Rectangle parent class"""
+
+
+class Square(Rectangle):
+    """Square class inherits from Rectangle"""
+
+    """instantiation with size"""
+    def __init__(self, size):
+        super().__init__(size, size)
