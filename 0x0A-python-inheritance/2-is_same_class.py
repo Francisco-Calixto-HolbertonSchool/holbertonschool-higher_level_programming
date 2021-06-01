71c51b454007bced45f154b5311930db35903f0d
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-01 17:41:50 +0000

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains declaration of function lookup"""
+
+
+def is_same_class(obj, a_class):
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
