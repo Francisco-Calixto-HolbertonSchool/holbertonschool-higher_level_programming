1e0b4a921d8173ee700fc6cfbf6aca98a9ef9631
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-01 19:34:25 +0000

    task 9 8

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,3 +15,11 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    """area method returns area duuuu"""
+    def area(self):
+        return (self.__width * self.__height)
+
+    """__str__ method to return when str()"""
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
