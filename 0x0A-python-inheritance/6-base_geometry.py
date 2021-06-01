89fbd98345eb0665677be536aee5b10cd8c321cc
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-01 17:58:17 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains BaseGeometry class"""
+
+
+class BaseGeometry(list):
+    """Class BaseGeometry, for now does nothing"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
