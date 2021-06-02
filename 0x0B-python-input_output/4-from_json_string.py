9e603e0fde88cb7627d1d34b8b5fb258539e6ce6
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 15:13:46 +0000

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""modules containing that reads text and prints"""
+
+
+import json
+"""import jasonn gg"""
+
+
+def from_json_string(my_str):
+    """write text to file, overwrite if needed"""
+    return (json.loads(my_str))
