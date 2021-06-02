37fd2e60d10b788c45476d023e552cb359db12e9
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 01:29:08 +0000

    attttt

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+"""module commenting yomama"""
+
+
+"""set atr atr"""
+def add_attribute(obj, name, value):
+    setattr(obj, name, value)
+    if not hasattr(obj, name):
+        raise TypeError("can't add new attribute")
