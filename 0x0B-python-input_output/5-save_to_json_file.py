a018029a48931733c8edafa4b0c242d3a32a60e2
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 15:33:58 +0000

    task5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+"""modules containing that reads text and prints"""
+
+
+import json
+"""import jasonn gg"""
+
+
+def save_to_json_file(my_obj, filename):
+    """write text to file, overwrite if needed"""
+    with open(filename, 'w') as json_f:
+        json_f.write(json.dumps(my_obj))
