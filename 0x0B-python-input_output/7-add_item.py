0a7dd92b7af46bcd2ec73245b85e9016c4280fb0
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 21:50:08 +0000

    permissions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """import jasonn utilities"""
 
 
-def main():
-    """main function"""
-    new_args = []
-    actual = load_from_json_file("add_item.json")
-    for i in range(1, len(argv)):
-        new_args.append(argv[i])
-    new_lis = actual + new_args
-    save_to_json_file(new_lis, "add_item.json")
+new_args = []
+actual = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    new_args.append(argv[i])
+new_lis = actual + new_args
+save_to_json_file(new_lis, "add_item.json")
