8b059e8aa305c43ad93e8da9f4670e2a569b5fdf
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 21:38:52 +0000

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""modules containing that reads text and prints"""
+
+
+import json
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""import jasonn utilities"""
+
+new_args = []
+actual = load_from_json_file("add_item.json")
+for i in range(1, len(argv)):
+    new_args.append(argv[i])
+new_lis = actual + new_args
+save_to_json_file(new_lis, "add_item.json")
