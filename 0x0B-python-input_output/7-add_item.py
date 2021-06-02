f0d9df76fe5a13897b101b51490855ae6a98b8e9
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 21:52:23 +0000

    permissions

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import json
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""import jasonn utilities"""
+"""import jssasonn utilities"""
 
 
 new_args = []
