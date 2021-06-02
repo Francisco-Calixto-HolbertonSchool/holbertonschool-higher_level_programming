008863c60f568acbb341f9ba2484bae8317c072b
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 14:21:47 +0000

    task 0 and readme

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""modules containing that reads text and prints"""
+
+
+def read_file(filename=""):
+    """reads and prints from txt"""
+    with open(filename, 'r', encoding = "UTF8") as f:
+        for line in f:
+            print(line, end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+0x0B. Python - Input/Output
+
+Python project focusing on opening, reading and writing files
