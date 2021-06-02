19b8668c0fc47c20319471421a3c1ba416ae2a60
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 15:55:40 +0000

    loads

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pyhton3
+"""modules containing that reads text and prints"""
+
+
+import json
+"""import jasonn gg"""
+
+
+def load_from_json_file(filename):
+    """write text to file, overwrite if needed"""
+    with open(filename, 'r') as f:
+        return (json.loads(f.read()))
