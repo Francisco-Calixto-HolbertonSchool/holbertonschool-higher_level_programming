4235e26ad9e90243e1826512da37243cbcd2758d
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 22:04:52 +0000

    aaaa

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,13 +3,16 @@
 
 
 import json
+from os import path
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 """import jssasonn utilities"""
 
-
-actual = load_from_json_file("add_item.json")
+if path.isfile("add_item.json"):
+    actual = load_from_json_file("add_item.json")
+else:
+    actual = []
 for i in range(1, len(argv)):
     actual.append(argv[i])
 save_to_json_file(actual, "add_item.json")
