0297ae111918d20dda92bfa926642aa37c16ffa7
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 14:46:15 +0000

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyhton3
+"""modules containing that reads text and prints"""
+
+
+def append_write(filename="", text=""):
+    """reads and prints from txt"""
+    with open(filename, 'a', encoding = "UTF8") as new_file:
+        new_file.write(text)
+    return (len(text))
