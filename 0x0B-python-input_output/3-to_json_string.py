5b424ef0c3a7e7ae2e8bdb5df2055ab52b61aa89
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-02 15:11:35 +0000

    task3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/pyhton3
+"""modules containing that reads text and prints"""
+
+
+import json
+"""import jasonn gg"""
+
+
+def to_json_string(my_obj):
+    """write text to file, overwrite if needed"""
+    return (json.dumps(my_obj))
