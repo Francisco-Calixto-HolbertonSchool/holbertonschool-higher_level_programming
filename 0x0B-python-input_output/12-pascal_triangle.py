c7247803b7d13ee63aab9b37e6d4445458ac430f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-03 01:18:25 +0000

    task12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""modules containing that reads text and prints"""
+
+
+def pascal_triangle(n):
+    """write text to file, overwrite if needed"""
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        a = 11 ** i
+        row = [int(digit) for digit in str(a)]
+        triangle += [row]
+    return triangle
