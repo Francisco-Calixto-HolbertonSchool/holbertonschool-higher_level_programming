66bbafda2c74fbb50fe4e46ddddb1d3587685280
Francisco-Calixto-HolbertonSchool <77985090+Francisco-Calixto-HolbertonSchool@users.noreply.github.com>
2021-06-06 10:44:39 -0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,6 +16,12 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    def update(self, *args):
+        """assigns an argument to each attribute"""
+        aux = [self.id, self.width, self.height, self.x, self.y]
+        for i in range(len(args)):
+            aux[i] = args[i]
+
     @property
     def width(self):
         """useles coment"""
@@ -98,9 +104,3 @@ class Rectangle(Base):
         """change the str"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
-
-    def update(self, *args):
-        """assigns an argument to each attribute"""
-        aux = [self.id, self.width, self.height, self.x, self.y]
-        for i in range(len(args)):
-            aux[i] = args[i]
