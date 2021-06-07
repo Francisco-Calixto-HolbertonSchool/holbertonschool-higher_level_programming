cf0345d88bd53c07c80a2004f020787a9dddaf15
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-07 19:07:24 +0000

    everytih

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,3 +23,9 @@ class Base():
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """mi pan """
+        if list_objs is None:
+            
