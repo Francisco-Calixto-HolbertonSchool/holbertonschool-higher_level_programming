ce9080d98b6eaaf6f3e057f0c4aa8dcc36ee35c0
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-06-07 17:40:23 +0000

    cleanup

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,3 +26,36 @@ class Square(Rectangle):
         """size property ge"""
         self.width = v
         self.heiht = v
+
+    def update(self, *args, **kwargs):
+        """updates values of the Square instance from args or kwargs"""
+        if args:
+            l = len(args)
+            self.id = args[0]
+            i = 1
+            if i < l:
+                self.size = args[i]
+                i += 1
+            if i < l:
+                self.x = args[i]
+                i += 1
+            if i < l:
+                self.y = args[i]
+        elif kwargs:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """to ionar ydicto"""
+        dic = {}
+        dic['id'] = self.id
+        dic['size'] = self.size
+        dic['x'] = self.x
+        dic['y'] = self.y
+        return dic
