83a5cd880e38f74880c42c393ae7b168ad4a6654
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-02 13:28:46 -0300

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ require('process');
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(process.argv[2]));
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
