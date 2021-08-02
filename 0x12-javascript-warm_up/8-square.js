11275274e48578b2bf22dba9a6188764c9c0cd17
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-02 14:13:20 -0300

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -7,7 +7,7 @@ const size = process.argv[2];
 if (size === undefined || isNaN(parseInt(size))) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i ++) {
+  for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
 }
