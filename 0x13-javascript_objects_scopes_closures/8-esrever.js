047d99272e8ecb5aaf3787800cd633709941d075
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-03 13:00:56 -0300

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  const l = list.length - 1;
+  const arrcpy = [];
+  for (let i = l; i > 0; i--) {
+    arrcpy.push(list[i]);
+  }
+  return arrcpy;
+};
