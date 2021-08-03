b5d8d846c4237d0513fdec4efd515f6b0a10375f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-03 12:49:44 -0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,7 +9,9 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      this.print();
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
     } else {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
