714c46b862fab586f098aa6261fa104986c72209
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-06 11:20:27 -0300

    a

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,6 +10,5 @@ if __name__ == "__main__":
     sql = "SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s"
     cursor.execute(sql, (argv[4],))
     res = cursor.fetchall()
-    for row in res:
-        print(row)
+    print(res)
     db.close()
