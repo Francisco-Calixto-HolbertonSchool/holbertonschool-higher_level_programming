3b194e29bd4fc90cab22df2567c46488ef6d2027
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-05 23:44:38 -0300

    task 3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''all values of states table (hbtn_0e_0_usa) that name matches argument'''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3], 3306)
+    cursor = db.cursor()
+    sql = 'SELECT * FROM states ORDER BY id'
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    for row in res:
+        if row[1] == argv[4]:
+            print(row)
+    db.close()
