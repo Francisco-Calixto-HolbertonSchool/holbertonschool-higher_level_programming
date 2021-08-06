84d3e84e1a0e5173408bd46c30ae75b80e1f939c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-06 10:25:32 -0300

    a

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3], 3306)
     cursor = db.cursor()
     sql = "SELECT * FROM states WHERE name = %s"
-    cursor.execute(sql, (name,))
+    cursor.execute(sql, (argv[4],))
     res = cursor.fetchall()
     for row in res:
         if row[1] == argv[4]:
