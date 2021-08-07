0db9c7876f9b4b5b1ed731fdcbae8fc1cab18627
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-07 12:39:41 -0300

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,8 @@ from sys import argv
 if __name__ == '__main__':
     db = MySQLdb.connect('localhost', argv[1], argv[2], argv[3], 3306)
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name = %s"
-    cursor.execute(sql, (argv[4],))
+    sql = "SELECT * FROM states WHERE name = '{}'".format(argv[4])
+    cursor.execute(sql)
     res = cursor.fetchall()
     for row in res:
         if row[1] == argv[4]:
