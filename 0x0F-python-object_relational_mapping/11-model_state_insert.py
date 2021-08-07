94a3b07d9d32a38b35f04c287e920440ece15418
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-07 16:37:15 -0300

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''prints the first State object from the database hbtn_0e_6_usa'''
+
+import sqlalchemy
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+        )
+    Base.metadata.create_all(engine)
+    session = sqlalchemy.orm.Session(engine)
+    martin = State(name='Louisiana')
+    session.add(martin)
+    print(session.query(State).filter_by(name='Louisiana').all()[0].id)
+    session.close()
