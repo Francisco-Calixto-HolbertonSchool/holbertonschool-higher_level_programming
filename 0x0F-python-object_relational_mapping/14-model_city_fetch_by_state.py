6a4270e9a959bc35e2b1991b090f3539ae8520b6
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-07 20:29:42 -0300

    pep8

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
         )
     Base.metadata.create_all(engine)
     session = sqlalchemy.orm.Session(engine)
-    for st, ci in session.query(State, City).filter(State.id == City.state_id).all():
+    for st, ci in session.query(State, City).filter(
+        State.id == City.state_id
+        ).all():
         print('{}: ({}) {}'.format(st.name, ci.id, ci.name))
     session.close()
