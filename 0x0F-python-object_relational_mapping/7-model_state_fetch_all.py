518dce31a11780cc48cccd150b79403de0914113
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-07 11:51:33 -0300

    orm

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 if __name__ == '__main__':
     engine = sqlalchemy.create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
-    session = sqlalchemy.Session(engine)
+    session = sqlalchemy.orm.Session(engine)
     for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
         print("{}: {}".format(state.id, state.name))
     session.close()
