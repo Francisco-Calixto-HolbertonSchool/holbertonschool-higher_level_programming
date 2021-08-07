d85a66941fd0798beca2f221e0f400b5039ea71e
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-07 12:20:54 -0300

    ?

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''prints the first State object from the database hbtn_0e_6_usa'''
+
+import sqlalchemy
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    session = sqlalchemy.orm.Session(engine)
+    for state in session.query(State).order_by(State.id).exists(argv[4]):
+        print(state.id)
+    session.close()
