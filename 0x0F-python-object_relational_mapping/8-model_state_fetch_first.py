4955f8a8694fd1d594318c0b0cb13280ec5adb18
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-08-07 13:44:56 -0300

    8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,7 +9,5 @@ if __name__ == "__main__":
     engine = sqlalchemy.create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     session = sqlalchemy.orm.Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-        break
+    session.query(State).order_by(State.id).all()[0]
     session.close()
