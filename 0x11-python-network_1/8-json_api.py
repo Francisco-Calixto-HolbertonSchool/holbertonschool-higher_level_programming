22c238e9b24b2f8379e04006bd0687f978bf0880
coding-kiko <2789@holbertonschool.com>
2021-08-20 23:27:31 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''request url and get variable'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    try:
+        q = argv[1]
+    except:
+        q = ""
+    params = {'q': q}
+    request = requests.post('http://0.0.0.0:5000/search_user', data=params)
+    try:
+        j = request.json()
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
+    else:
+        if j != {}:
+            print("[{}] {}".format(j.get('id'), j.get('name')))
+        else:
+            print("No result")
