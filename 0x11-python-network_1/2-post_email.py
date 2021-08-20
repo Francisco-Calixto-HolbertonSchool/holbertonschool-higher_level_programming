3784de9210fad38031f696e7db4563e5fe0159df
coding-kiko <2789@holbertonschool.com>
2021-08-20 17:05:29 +0000

    pep 8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,11 +6,10 @@ import urllib.parse
 from sys import argv
 
 if __name__ == "__main__":
-    params = {'email' : argv[2]}
+    params = {'email': argv[2]}
     data = urllib.parse.urlencode(params)
     data = data.encode('ascii')
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as res:
         body = res.read()
         print(body.decode('utf-8'))
-
