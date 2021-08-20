c4f9514708256bacb7b35d8f0671385535da9bcf
coding-kiko <2789@holbertonschool.com>
2021-08-20 22:21:31 +0000

    task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''request url and get variable'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    print(response.headers["X-Request-Id"])
