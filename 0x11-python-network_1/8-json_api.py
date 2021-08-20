69b7f504239d41a76ce9dddaf18a391fb670f2d9
coding-kiko <2789@holbertonschool.com>
2021-08-20 23:33:36 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     params = {'q': q}
     request = requests.post('http://0.0.0.0:5000/search_user', data=params)
     try:
-        request.json()
+        j = request.json()
     except requests.exceptions.JSONDecodeError:
         print('Not a valid JSON')
     else:
         if request.json() != {}:
-            print("[{}] {}".format(request.json().get('id'), request.json().get('name')))
+            print("[{}] {}".format(j.get('id'), j.get('name')))
         else:
             print("No result")
