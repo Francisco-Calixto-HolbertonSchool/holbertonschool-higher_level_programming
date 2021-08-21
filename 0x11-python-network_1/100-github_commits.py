64b41b310e3651b5ed4f8b060b64b67e45a41ab6
coding-kiko <2789@holbertonschool.com>
2021-08-21 01:21:08 +0000

    Dtask advanced sacalaaa

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''request url and get variable'''
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo = argv[1]
+    user = argv[2]
+    url = "https://api.github.com/repos/" + user + '/' + repo + "/commits"
+    response = requests.get(url)
+    for i in range(10):
+        sha = response.json()[i].get('sha')
+        author = response.json()[i].get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
