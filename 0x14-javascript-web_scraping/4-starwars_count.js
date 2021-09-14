84e4f41098dd2dbb7721733f6aadd21bad9c50a5
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-14 14:38:13 -0300

    ss

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,13 @@ require('process');
 const request = require('request');
 const url = process.argv[2];
 let count = 0;
-const results = {};
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
     return;
   }
-  results = JSON.parse(body).results;
+  const results = JSON.parse(body).results;
   for (let i = 0; i < results.length; i++) {
     for (let j = 0; j < results[i].characters.length; j++) {
       if (results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
