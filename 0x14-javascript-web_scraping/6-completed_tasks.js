f870b173bd56cb740c891909db7aea9c52c31677
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-14 16:02:36 -0300

    s

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -14,9 +14,8 @@ request(url, function (error, response, body) {
     for (task in r) {
         if (!(task.userId in output) && task.completed) {
             output[task.userId] = 1;
-            continue;
         }
-        if (task.completed) {
+        else if (task.completed) {
             output[task.userId] += 1;
             continue;
         }
