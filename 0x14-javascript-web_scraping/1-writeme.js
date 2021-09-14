ff99a402bf32bc6b76c7d62015dc0c1511a3e7dd
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-14 11:29:41 -0300

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+require('process');
+const fs = require('fs')
+
+fs.writeFile(process.argv[2], process.argv[3] , {
+        encoding: "utf8",
+        flag: "w",
+        mode: 0o666
+    }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  })
