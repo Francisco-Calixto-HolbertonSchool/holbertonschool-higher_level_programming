cc5f7094ea30fed97b592540bb2f13a9081a2672
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-14 16:34:42 -0300

    semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,14 +6,14 @@ const url = process.argv[2];
 const output = {};
 
 request(url, function (error, response, body) {
-	if (error) {
-  	console.log(error);
+  if (error) {
+    console.log(error);
     return;
   }
-	const r = JSON.parse(body);
+  const r = JSON.parse(body);
   for (let i = 0; i < r.length; i++) {
     if (!(r[i].userId in output) && r[i].completed) {
-    	output[r[i].userId.toString(10)] = 1;
+      output[r[i].userId.toString(10)] = 1;
     } else if (r[i].completed) {
       output[r[i].userId.toString(10)] += 1;
     }
