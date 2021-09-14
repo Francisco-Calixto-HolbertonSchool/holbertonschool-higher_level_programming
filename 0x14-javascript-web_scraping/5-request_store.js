dc2d05c1c95c492aece2ab0f5bb743b9d89cc65c
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-14 13:43:56 -0300

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+require('process');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+const fs = require('fs');
+
+request (url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFile(process.argv[2], process.argv[3], {
+    encoding: 'utf8',
+    flag: 'w',
+    mode: 0o666
+  }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
