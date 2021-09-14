1ed102149602dcf70ab10387dc41974903cd4e54
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-14 12:13:58 -0300

    semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,11 +3,11 @@
 require('process');
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3] , {
-    encoding: "utf8",
-    flag: "w",
-    mode: 0o666
-  }, (err) => {
+fs.writeFile(process.argv[2], process.argv[3], {
+  encoding: 'utf8',
+  flag: 'w',
+  mode: 0o666
+}, (err) => {
   if (err) {
     console.error(err);
   }
