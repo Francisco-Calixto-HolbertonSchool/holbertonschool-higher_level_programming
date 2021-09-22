38e3bff9001eb6dafc4529886c3c1283f62fcbd2
coding-kiko <2789@holbertonschool.com>
2021-09-22 18:38:11 +0000

    101

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', (arrow_funct) => {
+  $('DIV#add_item').click(function(){
+      $('UL.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').click(function(){
+      $('UL.my_list li:last-child').remove();
+  });
+  $('#clear_list').click(function(){
+      $('UL.my_list li').remove();
+  });
+});
