f9f15a9259c9c3a98df2395a9b997420601fdaf1
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-22 19:06:54 -0300

    semistandard

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 $.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', (data) => {
-    const name = data.name;
-    $('DIV#character').text(name);
+  const name = data.name;
+  $('DIV#character').text(name);
 });
