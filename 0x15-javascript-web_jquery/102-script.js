aa1ed04efd0dbe677d6277ceda081b196b92010f
Francisco-Calixto-HolbertonSchool <2789@holbertonschool.com>
2021-09-22 19:12:26 -0300

    semiSTandard

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (arrowfunct) => {
   $('#btn_translate').on('click', function () {
     const lang = $('#language_code').val();
     $.getJSON('https://fourtonfish.com/hellosalut/?lang=' + lang, (data) => {
-    $('#hello').text(data.hello);
+      $('#hello').text(data.hello);
     });
   });
 });
--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -5,8 +5,8 @@ window.addEventListener('DOMContentLoaded', (arrowfunct) => {
       $('#hello').text(data.hello);
     });
   });
-  $('#language_code').on('keypress', function(e) {
-    if(e.which === 13) {
+  $('#language_code').on('keypress', function (e) {
+    if (e.which === 13) {
       const lang = $('#language_code').val();
       $.getJSON('https://fourtonfish.com/hellosalut/?lang=' + lang, (data) => {
         $('#hello').text(data.hello);
